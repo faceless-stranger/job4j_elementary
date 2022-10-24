30c95fe91705017f04266dc58f6673ed1dafec7c
PC <faceless-stranger@mail.ru>
2022-10-24 18:58:55 +0300

    3. Арифметические операции. [#185]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,6 +2,14 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет мир");
+        byte age = 25;
+        char pol = 'М';
+        float rost = 1.95f;
+        long koe = 9999999;
+        short kirpich = 24500;
+        int moon = 400000;
+        boolean job = true;
+        short sizefile = 25392;
+
     }
 }
