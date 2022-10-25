ab1ec54a9ccdac54a33a7274caea01f0fb3a43fa
PC <faceless-stranger@mail.ru>
2022-10-25 17:51:53 +0300

    4.2. Результат работы метода [#232622]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,15 +1,12 @@
 package ru.job4j;
 
 public class Main {
+    public static int func1(int x) {
+        int y = x * x + 1;
+        return y;
+    }
     public static void main(String[] args) {
-        byte age = 25;
-        char pol = 'М';
-        float rost = 1.95f;
-        double koe = 1E7;
-        short kirpich = 24500;
-        double moon = 400000;
-        boolean job = true;
-        long sizefile = 25_392L;
-
+        int result3 = Main.func1(100);
+        System.out.println( result3 );
     }
 }
