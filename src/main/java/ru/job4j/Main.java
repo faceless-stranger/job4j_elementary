add85f8e701717832aa1d8d093fb809388798702
PC <faceless-stranger@mail.ru>
2022-10-26 20:00:29 +0300

    4.2. Результат работы метода [#232622 #354944]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,16 +1,17 @@
 package ru.job4j;
 
 public class Main {
-    public static void hello(String name, int age) {
-        System.out.println("Hello, " + name + ", age = " + age);
+    public static int func1(int x) {
+        int y = x * x + 1;
+        return y;
     }
 
     public static void main(String[] args) {
-        String name = "Job4j";
-        int age = 6;
-        Main.hello(name, age);
-        Main.hello(name, age);
-        Main.hello(name, age);
-        Main.hello(name, age);
+        int result3 = Main.func1(100);
+        System.out.println(result3);
+    }
+
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", age = " + age);
     }
 }
