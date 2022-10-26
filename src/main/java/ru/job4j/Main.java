1e35a67784c8d6995bdea12e5b087de6b2451287
PC <faceless-stranger@mail.ru>
2022-10-26 19:56:09 +0300

    4.1. Аргументы [#295521 #354933]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,16 +1,6 @@
 package ru.job4j;
 
 public class Main {
-//    public static int func1(int x) {
-//        int y = x * x + 1;
-//        return y;
-//    }
-//
-////    public static void main(String[] args) {
-////        int result3 = Main.func1(100);
-////        System.out.println(result3);
-////    }
-
     public static void hello(String name, int age) {
         System.out.println("Hello, " + name + ", age = " + age);
     }
