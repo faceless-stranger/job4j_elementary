593d0b5cdcd7d776690d18b1797b22457dc1e882
PC <faceless-stranger@mail.ru>
2022-10-30 16:02:25 +0300

    9.2. Математические операторы и составные операторы присваивания [#504905]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -12,4 +12,7 @@ public class SqArea {
         double result1 = SqArea.square(6, 2);
         System.out.println(" p = 6, k = 2, s = 1, real = " + result1);
     }
+
+    public static class StringRunner {
+    }
 }
