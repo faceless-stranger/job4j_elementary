0da05117126c45a128e1a57b4063f7690f2ea64a
PC <faceless-stranger@mail.ru>
2022-10-30 15:00:05 +0300

    9.1. String [#237407 #356106]

--- src/main/java/ru/job4j/Condition.java
+++ src/main/java/ru/job4j/Condition.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Condition {
     public static void main(String[] args) {
- String idea = "I like Java!";
+        String idea = "I like Java!";
         System.out.println(idea);
         idea += "But I am a newbie.";
         int year = 2022;
