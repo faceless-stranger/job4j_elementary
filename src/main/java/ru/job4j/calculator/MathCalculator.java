dc2077352d6c6f11d3edb7b7a892c5a34c090096
PC <faceless-stranger@mail.ru>
2022-11-01 23:24:34 +0300

    Исправление ошибок

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -10,6 +10,6 @@ public class MathCalculator {
     }
 
     public static void main(String[] args) {
-        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен:  " + sumAndMultiply(10, 20));
     }
 }
