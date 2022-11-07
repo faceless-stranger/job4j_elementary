61f5d7a8d0356fa6ea68b15d324977fd346a39c4
PC <faceless-stranger@mail.ru>
2022-11-08 00:47:34 +0300

    исправление ошибки

--- src/main/java/ru/job4j/loop/Board.java
+++ src/main/java/ru/job4j/loop/Board.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class Board {
     public static void paint(int width, int height) {
         for (int i = 0; i < height; i++) {
-            for (int j = 0; j <= width; j++ ) {
+            for (int j = 0; j <= width; j++) {
                 /* условие проверки, что писать пробел или X */
                 /* Выше в задании мы определили закономерность, когда нужно проставлять X */
                 if ((j + i) % 2 == 0) {
