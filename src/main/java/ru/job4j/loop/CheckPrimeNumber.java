7948faaed605cd7eb8ae4623c6c82b8b2d17afaa
PC <faceless-stranger@mail.ru>
2022-11-11 01:43:36 +0300

    5.5. Простое число [#171694]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static void main(String[] args) {
+        System.out.println(CheckPrimeNumber.check(5));
+    }
+
+    public static boolean check(int n) {
+        boolean base = 1 < n;
+        for (int i = 2; i < n - 1; i++) {
+            if (n % i == 0) {
+                base = false;
+                break;
+            }
+        }
+        return base;
+    }
+}
