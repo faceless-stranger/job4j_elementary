2aa01283b5b9caa77f4a2479c27a235c2e8ffedb
PC <faceless-stranger@mail.ru>
2022-11-14 22:05:34 +0300

    Исправление

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -3,11 +3,11 @@ package ru.job4j.array;
 public class ArrayLoop {
     public static void main(String[] args) {
         int[] numbers = new int[5];
-        for (int index = 0; numbers.length > index; index++) {
-        numbers[index] = index * 2 + 3;
-            for (int i  : numbers) {
-                System.out.println(i);
-            }
+        for (int index = 0; index < numbers.length; index++) {
+            numbers[index] = index * 2 + 3;
+        }
+        for (int index = 0; index < numbers.length; index++) {
+            System.out.println(numbers[index]);
         }
     }
 }
