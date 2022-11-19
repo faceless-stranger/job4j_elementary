2d651e070d10bb16d0b51716acc718b4094fd603
PC <faceless-stranger@mail.ru>
2022-11-19 21:14:29 +0300

    Исправление

--- src/test/java/ru/job4j/array/SwitchArrayTest.java
+++ src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -1,19 +1,37 @@
 package ru.job4j.array;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.assertj.core.api.Assertions.assertThat;
-
-class SwitchArrayTest {
+public class SwitchArrayTest {
 
     @Test
-    public void whenSwap0to3() {
-        int[] input = {1, 2, 3, 4};
+    public void whenSwap0To3() {
+        int[] input = new int[] {1, 2, 3, 4};
         int source = 0;
         int dest = input.length - 1;
         int[] result = SwitchArray.swap(input, source, dest);
-        int[] expected = {4, 2, 3, 1};
-        assertThat(result).containsExactly(expected);
+        int[] expected = new int[] {4, 2, 3, 1};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSwap3To6() {
+        int[] input = new int[] {3, 7, 3, 3, 7, 1, 9, 4, 2};
+        int source = 3;
+        int dest = 6;
+        int[] result = SwitchArray.swap(input, source, dest);
+        int[] expected = new int[] {3, 7, 3, 9, 7, 1, 3, 4, 2};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenNoSwap() {
+        int[] input = new int[] {2, 3, 0, 5};
+        int source = 1;
+        int dest = 2;
+        int[] result = SwitchArray.swap(input, source, dest);
+        int[] expected = new int[] {2, 0, 3, 5};
+        Assert.assertArrayEquals(expected, result);
     }
 }
