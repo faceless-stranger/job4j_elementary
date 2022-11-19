a4fd2174cc9588634c956ac568cb8ff5b74e8ee5
PC <faceless-stranger@mail.ru>
2022-11-19 20:41:27 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -11,4 +11,15 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+        int index = -1;
+        for (int i = start; i <= finish; i++) {
+            if (data[i] == el) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
 }
