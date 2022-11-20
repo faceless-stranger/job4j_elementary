b802a5c4404446eda45e6e40b8abbaaec1859d85
PC <faceless-stranger@mail.ru>
2022-11-20 18:57:52 +0300

    Исправление

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -2,9 +2,8 @@ package ru.job4j.array;
 
 public class Check {
     public static boolean mono(boolean[] data) {
-        boolean result = data[0];
         for (int index = 0; index < data.length; index++) {
-            if (data[index] != result) {
+            if (data[index] != data[0]) {
              return false;
             }
         }
