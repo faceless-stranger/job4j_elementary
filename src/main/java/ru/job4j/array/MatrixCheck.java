ec66196ee6312b281e4a1a3d0e00f391eca7ada4
PC <faceless-stranger@mail.ru>
2022-11-22 22:22:12 +0300

    Испрпвление

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -4,7 +4,7 @@ public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int cell = 0; cell < board[row].length; cell++) {
-            result = board[row][cell] == 'X' || board[row][cell] == ' ';
+            result = board[row][cell] == 'X';
             if (board[row][0] != board[row][cell]) {
                 result = false;
                 break;
